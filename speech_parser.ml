0e34a314a157d4214e563f8a433364eb44b81c2a
Garrison Shepard <shepard.garrisont@gmail.com>
2019-04-11 16:54:49 -0400

    feat: parse adj

--- speech_parser.ml
+++ speech_parser.ml
@@ -19,7 +19,16 @@ let read_all filename =
   in
   read []
 
+(* Read in names of files in directory. store them in a list. iter trhogh the list and call read_all. and each call to read all creates a list. add them to a list. after this you will iter thorugh this list and cons each elsit into 1. then put the list in a map *)
+(* m*)
 
+let argv = Array.to_list (Sys.argv)  
 
-let () = List.iter (fun x -> print_string x; print_string " \n") (read_all "tst2.txt")
+let rec build_adj ls = 
+   match ls with 
+   | hd::tl -> (read_all hd)@(build_adj tl)
+   | [] -> []
 
+let adj = build_adj argv
+
+let () = List.iter (fun x -> print_string x; print_string " \n") (adj)
