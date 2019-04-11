ac399ee87e588c1014c6c690cd2307645995103c
Garrison Shepard <shepard.garrisont@gmail.com>
2019-04-10 21:30:17 -0400

    feat: parse adj

---
+++ speech_parser.ml
@@ -0,0 +1,25 @@
+open Pervasives 
+
+let get_line ic =
+  try
+    Some (input_line ic)
+  with
+  | End_of_file -> None
+
+(* returns all lines from the file as a list of strings, arranged in the reverse order *)
+let read_all filename =
+  let ic = open_in filename in
+
+  let rec read acc =
+    match get_line ic with
+    | Some line -> read (line :: acc)
+    | None ->
+        close_in ic; (* close input channel *)
+        acc (* return accumulator *)
+  in
+  read []
+
+
+
+let () = List.iter (fun x -> print_string x; print_string " \n") (read_all "tst2.txt")
+
