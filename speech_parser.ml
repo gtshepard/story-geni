9147e3cce63a0e432ca4dd9549f2ff4ce14948c7
Garrison Shepard <shepard.garrisont@gmail.com>
2019-04-18 22:44:04 -0400

    chore: refractor code
    subject make parser into a module

--- speech_parser.ml
+++ speech_parser.ml
@@ -1,62 +1,75 @@
 open Pervasives 
 
-let get_line ic =
-  try
-    Some (input_line ic)
-  with
-  | End_of_file -> None
+module Parser = struct
+   
+  module Str = struct
+    type t = string
+    let compare = Pervasives.compare
+   end
 
-(* returns all lines from the file as a list of strings, arranged in the reverse order *)
-let read_all filename =
-  let ic = open_in filename in
-
-  let rec read acc =
-    match get_line ic with
-    | Some line -> read (line :: acc)
-    | None ->
-        close_in ic; (* close input channel *)
-        acc (* return accumulator *)
-  in
-  read []
-
-let rec combine = function 
-   | hd::tl -> (read_all hd)@(combine tl)
-   | [] -> []
-
-let make_dir_file = Sys.command "ls */ -d >> dir_names.txt"
-
-let dir = Array.to_list (Sys.readdir "adv")
-let curr_dir = Sys.chdir "adv"
-let adj = combine dir
+  module WordBank = Map.Make(Str)
   
-module Str = struct
-  type t = string
-  let compare = Pervasives.compare
+  module Int = struct 
+    type t = int 
+    let compare = Pervasives.compare
+  end
+
+  module WordId = Map.Make(Int)
+  
+  (*retrieves a line from a file*)
+  let get_line ic =
+    try
+      Some (input_line ic)
+    with
+    | End_of_file -> None
+
+  (* returns all lines from the file as a list of strings, arranged in the reverse order *)
+  let read_all filename =
+    let ic = open_in filename in
+      let rec read acc =
+        match get_line ic with
+        | Some line -> read (line :: acc)
+        | None ->
+          close_in ic; (* close input channel *)
+          acc (* return accumulator *)
+        in
+         read []
+  
+  (* combines contents of all filenames in the list*)
+  let rec combine = function 
+      | hd::tl -> (read_all hd)@(combine tl)
+      | [] -> []
+
+  (*TODO condense into function *)
+  (*generates a file with all subdirs in  name/  format  *)
+  let make_dir_file = Sys.command "ls */ -d >> dir_names.txt"
+
+  (*reads in all fie names for a given dir into a list*)
+  let dir = Array.to_list (Sys.readdir "adv")
+  
+  (* change to specified  dir to read files *)
+  let curr_dir = Sys.chdir "adv"
+  
+  let adj = combine dir
+  
+  let rec make_word_bank map ls = 
+     match ls with 
+    | [] -> map
+    | hd::tl -> make_word_bank (WordBank.add hd hd map) tl
+
+  let word_bank = make_word_bank WordBank.empty adj
+
+  let rec make_word_id map ls = 
+    match ls with 
+    | [] -> map
+    | hd::tl -> make_word_id (WordId.add (List.length tl) hd map) tl
+
+  let word_id = make_word_id WordId.empty adj
+
+  let print_word_id = WordId.iter (fun x y -> print_int x; print_string " "; print_string y; print_string " \n") word_id 
 end
 
-module WordBank = Map.Make(Str)
-
-let rec make_word_bank map ls = 
-   match ls with 
-   | [] -> map
-   | hd::tl -> make_word_bank (WordBank.add hd hd map) tl
-
-let word_bank = make_word_bank WordBank.empty adj
-
-module Int = struct 
-  type t = int 
-  let compare = Pervasives.compare
-end
-
-module WordId = Map.Make(Int)
-
-let rec make_word_id map ls = 
-  match ls with 
-  | [] -> map
-  | hd::tl -> make_word_id (WordId.add (List.length tl) hd map) tl
-
-let word_id = make_word_id WordId.empty adj
-
-let () = WordId.iter (fun x y -> print_int x; print_string " "; print_string y; print_string " \n") word_id
+let () = Parser.print_word_id
+  (*let () = WordId.iter (fun x y -> print_int x; print_string " "; print_string y; print_string " \n") word_id*)
   (*WordBank.iter (fun x y -> print_string x; print_string " "; print_string y; print_string " \n") word_bank*)
 
